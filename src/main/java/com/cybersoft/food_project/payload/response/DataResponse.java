dbdc358ab24e3838b5cdf9bbf9a54334f5e2ad89
Ken Pham <kpham@eyeprogpo.com>
2022-11-09 13:30:18 -0500

    feature/login

---
+++ src/main/java/com/cybersoft/food_project/payload/response/DataResponse.java
@@ -0,0 +1,40 @@
+package com.cybersoft.food_project.payload.response;
+
+public class DataResponse {
+    private int status;
+    private boolean isSuccess;
+    private String desc;
+    private Object data;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return isSuccess;
+    }
+
+    public void setSuccess(boolean success) {
+        isSuccess = success;
+    }
+}
