45727169e56af388227e554e89de6e9674e32c8d
Ken Pham <kpham@eyeprogpo.com>
2022-11-14 21:51:27 -0500

    feature/security

---
+++ src/main/java/com/cybersoft/food_project/security/SecSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.cybersoft.food_project.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig {
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService(){
+//        UserDetails user1 = User.withUsername("cybersoft")
+//                .password(passwordEncoder().encode("123"))
+//                .roles("USERS").build();
+//
+//        UserDetails user2 = User.withUsername("admin")
+//                .password(passwordEncoder().encode("admin123"))
+//                .roles("ADMIN").build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
+
+    @Autowired
+    CustomAuthentProvider customAuthentProvider;
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception{
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(customAuthentProvider);
+
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/signin").permitAll()
+                .antMatchers("/signin/test").authenticated()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
